916576645997a68e0fd521e54f7e38f59991b914
PriestOfFern <priestoffern@gmail.com>
2026-04-20 07:14:58 +0300

    fix compilation errors

--- src/main/java/org/valkyrienskies/horizons/impl/devices/components/StandardSocket.java
+++ src/main/java/org/valkyrienskies/horizons/impl/devices/components/StandardSocket.java
@@ -1,6 +1,56 @@
 package org.valkyrienskies.horizons.impl.devices.components;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import org.joml.Vector3dc;
+import org.joml.primitives.AABBdc;
 import org.valkyrienskies.horizons.api.devices.components.ISocket;
 
+import java.util.List;
+
 public class StandardSocket implements ISocket {
+    @Override
+    public BlockPos getBlockPos() {
+        return null;
+    }
+
+    @Override
+    public BlockEntity getBlockEntity() {
+        return null;
+    }
+
+    @Override
+    public Vector3dc getPos() {
+        return null;
+    }
+
+    @Override
+    public Vector3dc getNormal() {
+        return null;
+    }
+
+    @Override
+    public AABBdc getBounds() {
+        return null;
+    }
+
+    @Override
+    public List<ISocket> getConnectedSockets() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isConnectedTo(ISocket other) {
+        return false;
+    }
+
+    @Override
+    public boolean connect(ISocket other) {
+        return false;
+    }
+
+    @Override
+    public boolean disconnect(ISocket other) {
+        return false;
+    }
 }
--- src/main/kotlin/org/valkyrienskies/horizons/content/blockentities/DeviceBlockEntity.kt
+++ src/main/kotlin/org/valkyrienskies/horizons/content/blockentities/DeviceBlockEntity.kt
@@ -1,7 +1,18 @@
 package org.valkyrienskies.horizons.content.blockentities
 
+import net.minecraft.core.BlockPos
 import net.minecraft.world.level.block.entity.BlockEntity
+import net.minecraft.world.level.block.entity.BlockEntityType
+import net.minecraft.world.level.block.state.BlockState
+import org.valkyrienskies.horizons.api.devices.components.ISocket
 import org.valkyrienskies.horizons.api.devices.components.SocketedBlockEntity
 
-class DeviceBlockEntity : BlockEntity, SocketedBlockEntity {
+class DeviceBlockEntity(type: BlockEntityType<*>, pos: BlockPos, blockState: BlockState) : BlockEntity(type, pos, blockState), SocketedBlockEntity {
+    override fun getSocket(port: Int): ISocket? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getPorts(): Int {
+        TODO("Not yet implemented")
+    }
 }
